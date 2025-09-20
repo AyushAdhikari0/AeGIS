fc6e4e9c49183712c2a84f94ac4aede2e2d224e8
Ayush Adhikari <yush723@example.com>
2025-09-20 10:25:10 +0900

    camera position manager script

--- Assets/Scripts/CameraPositionManager.cs
+++ Assets/Scripts/CameraPositionManager.cs
@@ -10,62 +10,44 @@ public class CameraPositionManager : MonoBehaviour
 
     private float cameraFOV;
 
-    private float yaw = 0.0f;   // Y axis
-    private float pitch = 0.0f; // X axis
+    private float yaw = 0.0f;   // Y axis, yaw coordinate
+    private float pitch = 0.0f; // X axis, pitch coordinate
 
-    public float xSpeed = 120.0f;
-    public float ySpeed = 120.0f;
+    public float xSpeed = 120.0f;   // x and y-dir sensitivity
+    public float ySpeed = 120.0f; 
 
-    public float yMinLimit = -20f;
+    public float yMinLimit = -20f;  // min and max camera pitch 
     public float yMaxLimit = 80f;
 
-    public GameObject menuManager;
+    public GameObject menuManager;  
 
     public GameObject displayCoordinatesObject;
 
     private float previousTime;
-    public float timeInterval;
-
-
-    // private float currentMenu;
-
+    public float timeInterval;  // time interval to refresh drawing 2D and 3D fin mesh  
 
     void Start()
     {
-        // get camera FOV
+        // get initial camera angles
 
         Vector3 angles = transform.eulerAngles;
         yaw = angles.y;
         pitch = angles.x;
 
-
-        
-
     }
 
     void Update()
     {
-        
+        // updates camera position based on current menu and mouse input
         updateCameraPosition(GameObject.FindGameObjectWithTag("Menu Manager").GetComponent<MenuManager>().currentMenu);
            
     }
     // Update is called once per frame
     void updateCameraPosition(string currentMenu)
     {
-
-
-
-
         cameraFOV = gameObject.GetComponent<Camera>().fieldOfView;
-        List<Vector3> finCoords = gameObject.GetComponent<FinDimensionInputManager>().finCoords;
 
-
-        // find the fin mesh object if it exists
-
-        GameObject[] existingFinMeshes = GameObject.FindGameObjectsWithTag("Fin Mesh");
-
-        // Vector3 finCentre = gameObject.GetComponent<FinDimensionInputManager>().getFinCentre();
-
+        // calculate fin dimensions
         Vector3 finCenter;
         float halfFOV = cameraFOV / 2;
         float biggestDimension;
@@ -74,59 +56,45 @@ public class CameraPositionManager : MonoBehaviour
 
         if (currentMenu == "mainMenu")
         {
-            GameObject finMesh = existingFinMeshes[0];
-            // transform.LookAt(finMesh.transform);
+            GameObject finMesh = GameObject.FindGameObjectWithTag("Fin Mesh");
 
+            // get largest dimension and fin center from bounding box
             Bounds finBoundingBox = finMesh.GetComponent<MeshFilter>().mesh.bounds;
-            finCenter = finBoundingBox.center;
-
             Vector3 finBBDimensions = finBoundingBox.size;
 
             biggestDimension = finBBDimensions.magnitude;
+            finCenter = finBoundingBox.center;
 
-
-
+            // calculate minimum required distance to fit fin in camera FOV
             distanceAwayFromTarget = (float)(0.5f * biggestDimension / Math.Tan(halfFOV * Math.PI / 180));
 
-            if (Input.GetMouseButton(1)) // left mouse button held
+            // update yaw and pitch if left mouse button held
+            if (Input.GetMouseButton(1))
             {
                 yaw += Input.GetAxis("Mouse X") * xSpeed * Time.deltaTime;
                 pitch -= Input.GetAxis("Mouse Y") * ySpeed * Time.deltaTime;
                 pitch = Mathf.Clamp(pitch, yMinLimit, yMaxLimit);
             }
 
-            Quaternion rotation = Quaternion.Euler(pitch, yaw, 0f); // Z = 0 always
-
-            // Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
-            // Vector3 position = rotation * negDistance + target.position;
-
-            transform.rotation = rotation;
-            // transform.position = position;
+            // set new rotation and position
+            Quaternion newRotation = Quaternion.Euler(pitch, yaw, 0f); // Z = 0 always
+            transform.rotation = newRotation;
 
             transform.position = finCenter - transform.forward * (float)distanceAwayFromTarget;
             transform.LookAt(finCenter, transform.up);
 
-
-
-
-
-            // transform.rotation.z = 0;
-
+            // refresh fin mesh every time interval
             if (Time.time - previousTime > timeInterval)
             {
                 GameObject.FindGameObjectWithTag("MainCamera").GetComponent<FinDimensionInputManager>().generateFinMesh();
-
                 previousTime = Time.time;
-
-
             }
-
-
         }
+        
         else if (currentMenu == "selectDimensionMenu")
         {
-            // need to write code here that would centre the FOV of the camera at a 2D list of finCoords.
 
+            // get biggest dimension of the 2D fin geometry
             float maxX = 0;
             float maxY = 0;
             float minX = 0;
@@ -154,34 +122,24 @@ public class CameraPositionManager : MonoBehaviour
                 }
             }
 
+            // calculate required distance away from fin
             finCenter = new Vector3((maxX + minX) / 2, (maxY + minY) / 2, 0);
             biggestDimension = Math.Max(maxX - minX, maxY - minY);
             distanceAwayFromTarget = (float)(0.5 * biggestDimension / Math.Tan(halfFOV * Math.PI / 180));
 
+            // add padding
             distanceAwayFromTarget = distanceAwayFromTarget * 1.5f;
 
+            // set position and rotation
             transform.position = finCenter - Vector3.forward * (float)distanceAwayFromTarget;
             transform.LookAt(finCenter, Vector3.up);
-            
-                        if (Time.time - previousTime > timeInterval){
+
+            // refresh 2D fin geometry every time interval
+            if (Time.time - previousTime > timeInterval)
+            {
                 GameObject.FindGameObjectWithTag("Display Coordinates").GetComponent<DisplayCoordinates>().refreshEdgeCylindersAndFinCoords();
-
                 previousTime = Time.time;
-
-
             }
-
-
-
-
-
-
-
-
-
-        }
-            
-
-        
+        }   
     }
 }
